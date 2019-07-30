372ce460b4073a543e2986979d30710e39418c85
eugene94 <eugenekang94@gmail.com>
2019-07-30 16:54:34 +0900

    Feat: 초기 html

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 
 function App() {
   return (
-    <form>
-      할일입력 : <input name="todo" />
-      <button>등록</button>
-    </form>
+    <>
+      <form>
+        할일입력 : <input name="todo" />
+        <button>등록</button>
+      </form>
+      <div>
+        <h3>해야할 일들</h3>
+        <ul>
+          <li>공부하기</li>
+          <li>공부하기</li>
+          <li>공부하기</li>
+          <li>공부하기</li>
+        </ul>
+      </div>
+    </>
   );
 }
 
