f80eceb1e78e4ebc238050a2b1515db3eb186d72
eugene94 <eugenekang94@gmail.com>
2019-07-31 12:32:09 +0900

    Feat: styled-component
    - test로 app에서 Mainwrapper만 styled-component 적용

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+
+const MainWrapper = styled.div`
+  background-color: #dff9fb;
+`;
 
 class App extends Component {
   constructor(props) {
@@ -11,10 +16,10 @@ class App extends Component {
 
   render() {
     return (
-      <>
+      <MainWrapper>
         <TodoForm />
         <TodoList />
-      </>
+      </MainWrapper>
     );
   }
 }
