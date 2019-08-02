df1066d06ace977a9436f061b78c28a6628f40b3
eugene94 <eugenekang94@gmail.com>
2019-08-02 10:20:47 +0900

    Refactor: babel polyfill => runtime corejs
    deprecated된 babel@polyfill을 제거함
    preset의 useBuiltIns: usage사용으로 core-js3에 있는 polyfill사용
    전역 오염을 방지하고자 runtime시 polyfill을 적용하도록 함
    이를 위해 plugin-trnansform-runtime dependancy를 추가함

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   entry: {
-    app: ['@babel/polyfill', './src/index.js']
+    app: './src/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
