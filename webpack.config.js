85a144c4fb70880e6bc2526053a11ae8ed39ee5a
eugene94 <eugenekang94@gmail.com>
2019-08-02 12:12:07 +0900

    Refactor: babel & webpack 설정
    - cli 명령 간소화
    - mode conditional setting
    - babel preset target setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,40 @@
 const path = require('path');
-module.exports = {
-  entry: {
-    app: './src/index.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, options) => {
+  const config = {
+    entry: {
+      app: './src/index.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
         }
-      }
-    ]
+      ]
+    }
+  };
+
+  if (options.mode === 'development') {
+    config.devServer = { contentBase: path.join(__dirname, 'src') };
+    config.plugins = [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      })
+    ];
   }
+
+  if (options.mode === 'production') {
+    config.plugins = [new HtmlWebpackPlugin()];
+  }
+
+  return config;
 };
