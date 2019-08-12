6487ee0abfb59a21b66c018f1642921f5963363b
Jin Kang <eugenekang94@gmail.com>
2019-08-12 15:06:43 +0900

    [Jin] Step17 (#17)
    * Init: project setting
    
    - babel & webpack config
    - modules dependencies
    - public/html
    - src/index.js
    
    * Feat: app component 생성
    
    - 최상위 app 생성
    - 하위 form 생성
    
    * Feat: 초기 html
    
    * Refactor: 실행환경 변경
    
    - start:dev 시 content-base를 이용하여 build 파일과 무관하게 작동되도록
    함
    
    * Refactor: extracting-components
    
    - App / TodoList / TodoForm / Todo
    
    * Feat: styled-component
    
    - test로 app에서 Mainwrapper만 styled-component 적용
    
    * Feat: styled-normalize 적용
    
    * Style: TodoForm
    
    * Refactor: extract button component
    
    * Feat: Button onclick
    
    button onClick시 함수를 주입받아 실행할 수 있도록 만듦.
    todoForm Header스타일링도 같이 커밋
    
    * Feat: aysnc await 사용을 위한 babel/polyfill 추가
    
    * Feat: get api data / todo
    
    - todo data fetch로 불러옴
      - getData, getTodos 생성
    - getTodos(get todo component) 메서드 생성
    - todo component에 받아온 data인 title, id, staus 전달
    
    * Style: todo / li
    
    todo styling
    
    * Refactor: babel polyfill => runtime corejs
    
    deprecated된 babel@polyfill을 제거함
    preset의 useBuiltIns: usage사용으로 core-js3에 있는 polyfill사용
    전역 오염을 방지하고자 runtime시 polyfill을 적용하도록 함
    이를 위해 plugin-trnansform-runtime dependancy를 추가함
    
    * Feat: todo data fetch error handling
    
    * Refactor: babel & webpack 설정
    
    - cli 명령 간소화
    - mode conditional setting
    - babel preset target setting
    
    * Refactor: babel 설정 간소화
    
    runtime corejs3 => runtime
    
    * Refactor: TodoShow / TodoList 분리
    
    * Style: todoform input
    
    input border변경

---
+++ src/components/TodoForm.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import Button from './BasicButton';
+
+const Form = styled.form`
+  display: flex;
+  width: 100%;
+  box-sizing: border-box;
+
+  & :focus {
+    outline: none;
+  }
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  margin-left: 0.4rem;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  width: 3rem;
+  height: 1.5rem;
+  border: none;
+  border-bottom: double 3px #4a69bd;
+  transition: width 0.4s ease-in-out;
+
+  &:focus {
+    width: 14rem;
+    outline: none;
+  }
+`;
+
+const Label = styled.label`
+  line-height: 1.6;
+`;
+
+class TodoForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Form>
+        <Label htmlFor="todo">할 일</Label>
+        <InputWrapper>
+          <Input name="todo" id="todo" />
+          <Button onClick={e => e.preventDefault()}>등록</Button>
+        </InputWrapper>
+      </Form>
+    );
+  }
+}
+
+export default TodoForm;
