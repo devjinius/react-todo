95dda059eaa150558b6b49487676560ce2c0693a
eugene94 <eugenekang94@gmail.com>
2019-08-13 21:54:11 +0900

    Refactor: localAPI 구조 변경

--- localAPI/app.js
+++ localAPI/app.js
@@ -6,10 +6,11 @@ const app = express();
 app.use(cors());
 
 app.get('/todos', function(req, res) {
-  const data = fs.readFileSync('./todoData.json', { encoding: 'utf8' });
+  const data = JSON.parse(fs.readFileSync('./todoData.json', { encoding: 'utf8' }));
+  const result = { statusCode: 200, body: data };
 
   res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-  res.end(data);
+  res.end(JSON.stringify(result));
 });
 
 app.listen(3000, function() {
